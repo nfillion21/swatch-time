06c102cd6d75d82bc8f42e58f41a3fd94ed1db00
Nicolas Fillion <nfillion@lesechos.fr>
2024-09-13 17:07:49 +0200

    bumped version to 1.4.0 (build 5)

--- build-logic/convention/src/main/kotlin/AndroidApplicationConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/AndroidApplicationConventionPlugin.kt
@@ -24,8 +24,8 @@ private fun ApplicationExtension.configureDefaultConfig() {
     defaultConfig {
         applicationId = "xyz.poolp.swatchtime"
         targetSdk = 34
-        versionCode = 4
-        versionName = "1.3"
+        versionCode = 5
+        versionName = "1.4.0"
 
         vectorDrawables {
             useSupportLibrary = true
